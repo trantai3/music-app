cedcdcee856e075d954bc1eb60836f401eae0aba
trantai3 <trantai09032003@gmail.com>
2024-10-06 20:37:54 +0700

    get topic

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv"
 
 import * as database from "./config/database"
 
+import Topic from './models/topic.model'
+
 dotenv.config()
 
 database.connect()
@@ -14,7 +16,13 @@ const port: number | string = process.env.PORT || 3000
 app.set("views", "./views")
 app.set("view engine", "pug")
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+    const topic = await Topic.find({
+        deleted: false
+    })
+
+    console.log(topic)
+    
     res.render("client/pages/topics/index")
 })
 
