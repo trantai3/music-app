4a9b8ff3699edbd3f7bf90aca1f0e1e15d2b47be
trantai3 <trantai09032003@gmail.com>
2024-10-06 21:28:38 +0700

    create songs topic page

--- controller/client/topic.controller.ts
+++ controller/client/topic.controller.ts
@@ -7,9 +7,9 @@ export const topics = async (req: Request, res: Response) => {
     const topics = await Topic.find({
         deleted: false
     })
-
     res.render("client/pages/topics/index", {
-        pageTitle: "Chủ đề bài hát"
+        pageTitle: "Chủ đề bài hát",
+        topics: topics
     })
 
 }
