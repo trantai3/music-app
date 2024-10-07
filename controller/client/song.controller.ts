c119d5ed883f79ed5185ca0447d22412db2de2a2
trantai3 <trantai09032003@gmail.com>
2024-10-07 15:43:48 +0700

    create like feature

--- controller/client/song.controller.ts
+++ controller/client/song.controller.ts
@@ -61,3 +61,28 @@ export const detail = async (req: Request, res: Response) => {
         topic: topic
     })
 }
+
+// [PATCH] /songs/like/:typeLike/:idSong
+export const like = async (req: Request, res: Response) => {
+    const idSong: string = req.params.idSong
+    const typeLike: string = req.params.typeLike
+
+    const song = await Song.findOne({
+        _id: idSong,
+        status: "active",
+        deleted: false
+    })
+
+    const newLike: number = typeLike == "like" ? song.like + 1 : song.like - 1
+
+    await Song.updateOne({
+        _id: idSong
+    }, {
+        like: newLike
+    })
+    res.json({
+        code: 200,
+        message: "Thành công!",
+        like: newLike
+    })
+}
