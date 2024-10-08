4ff7ff96e9c467c60ea0dd30aeec96fed1d2965c
trantai3 <trantai09032003@gmail.com>
2024-10-08 21:43:30 +0700

    layout of the create new song page

--- controller/admin/song.controller.ts
+++ controller/admin/song.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 
 import Song from "../../models/song.model"
+import Topic from "../../models/topic.model"
+import Singer from "../../models/singer.model"
 
 // [GET] /admin/songs/
 export const index = async (req: Request, res: Response) => {
@@ -11,4 +13,22 @@ export const index = async (req: Request, res: Response) => {
     pageTitle: "Quản lý bài hát",
     songs: songs
   })
+}
+
+// [GET] /admin/songs/create
+export const create = async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false,
+        status: "active"
+    }).select("title")
+
+    const singers = await Singer.find({
+        status: "active",
+        deleted: false
+    }).select("fullName")
+    res.render("admin/pages/songs/create", {
+        pageTitle: "Thêm mới bài hát",
+        topics: topics,
+        singers: singers
+      })
 }
--- routes/admin/song.route.ts
+++ routes/admin/song.route.ts
@@ -1,3 +1,4 @@
+import { create } from './../../../task-management-ts/api/v1/controllers/task.controller';
 import { Router } from "express"
 
 const router: Router = Router()
@@ -5,5 +6,8 @@ const router: Router = Router()
 import * as controller from "../../controller/admin/song.controller"
 
 router.get('/', controller.index)
+
+router.get('/create', controller.create)
+
 
 export const songRoutes: Router = router
