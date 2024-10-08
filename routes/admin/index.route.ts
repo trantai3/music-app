c413553e725e017fcb5ae344b25d9dd0ce40c0ed
trantai3 <trantai09032003@gmail.com>
2024-10-08 21:11:41 +0700

    theme management page

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -4,11 +4,16 @@ import { dashboardRoutes } from "./dashboard.route"
 
 import { systemConfig } from "../../config/config"
 
+import { topicRoutes } from "./topic.route"
+
 const adminRoutes = (app: Express): void => {
 
     const PATH_ADMIN = `/${systemConfig.prefixAdmin}`
-    
+
     app.use(`${PATH_ADMIN}/dashboard`, dashboardRoutes)
+
+    app.use(`${PATH_ADMIN}/topics`, topicRoutes)
+
 
 }
 
