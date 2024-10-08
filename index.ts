dcfafe5fec25c7ae980e0734790247880f564737
trantai3 <trantai09032003@gmail.com>
2024-10-08 20:59:14 +0700

    layout in admin side

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ import * as database from "./config/database"
 
 import clientRoutes from './routes/client/index.route'
 
+import adminRoutes from './routes/admin/index.route'
+import { systemConfig } from './config/config'
+import path from "path"
+
 dotenv.config()
 
 database.connect()
@@ -17,6 +21,19 @@ app.use(express.static("public"))
 
 app.set("views", "./views")
 app.set("view engine", "pug")
+
+// TinyMCE
+app.use(
+    "/tinymce",
+    express.static(path.join(__dirname, "node_modules", "tinymce"))
+  );
+  // End TinyMCE
+
+// App local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+// Admin Routes
+adminRoutes(app)
 
 // Client Routes
 clientRoutes(app)
---
+++ routes/admin/dashboard.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express"
+
+const router: Router = Router()
+
+import * as controller from "../../controller/admin/dashboard.controller"
+
+router.get('/', controller.index)
+
+export const dashboardRoutes: Router = router
